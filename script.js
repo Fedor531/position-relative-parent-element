ab30d41eb87d0fdf4dd354156e96d7becb850d75
Федор <fedor531@yandex.ru>
2021-03-19 15:25:01 +0700

    add script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const pointsArray = [
     id: 1,
     x: 5,
     y: 20,
-    color: 'red',
+    color: 'aqua',
   },
   {
     id: 2,
@@ -21,7 +21,7 @@ const pointsArray = [
   },
 ];
 
-window.onload = function () {
+function renderPoints() {
   pointsArray.forEach((item) => {
     const point = document.createElement('div');
     point.classList.add('point');
@@ -32,4 +32,29 @@ window.onload = function () {
     );
     parent.insertAdjacentElement('beforeend', point);
   });
-};
+}
+
+/* Вывод элементов из массива на страницу */
+window.onload = renderPoints;
+
+parent.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('point')) {
+      console.log('hello');
+    /* Чтобы элемент ставился по центру там где кликнули нужно */
+    const newPointTop = Number(
+      (((e.offsetY - 5) * 100) / parent.offsetHeight).toFixed(3)
+    );
+    const newPointLeft = Number(
+      (((e.offsetX - 5) * 100) / parent.offsetWidth).toFixed(3)
+    );
+
+    // Формируем объект нового поинта
+    const newPoint = {};
+    newPoint.id = pointsArray.length + 1;
+    newPoint.x = newPointLeft;
+    newPoint.y = newPointTop;
+    newPoint.color = 'red';
+    pointsArray.push(newPoint);
+    renderPoints();
+  }
+});
